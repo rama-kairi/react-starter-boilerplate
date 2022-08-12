fb013266424c021a985af124af600c8e419c6927
Ramananda Kairi <ramanandakairi@gmial.com>
2022-08-12 18:25:05 +0530

    feat(cicd): release yml updated -1

--- release.config.ts
+++ release.config.ts
@@ -17,25 +17,5 @@ export default {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-      },
-    ],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md'],
-        message:
-          '${nextRelease.version} CHANGELOG [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
-    [
-      '@semantic-release/exec',
-      {
-        successCmd: 'echo"SEMVER_VERSION=${nextRelease.version}" > $GITHUB_ENV',
-      },
-    ],
   ],
 }

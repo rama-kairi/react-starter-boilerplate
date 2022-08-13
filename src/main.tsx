b6afde4a85121546ea61ba740e79b393fa28a67b
Ramananda Kairi <ramanandakairi@gmial.com>
2022-08-13 11:36:40 +0530

    feat(mantine): mantine package added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
+import { MantineProvider } from '@mantine/core'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 )

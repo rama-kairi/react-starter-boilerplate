66acb64f152957dce7df7870f7eafc0724d8d300
Ramananda Kairi <ramanandakairi@gmial.com>
2022-08-16 12:04:08 +0530

    fix(abs): absolute import jest issues resolved

--- jest.config.ts
+++ jest.config.ts
@@ -7,9 +7,14 @@ const config: Config.InitialOptions = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    'src/(.*)': '<rootDir>/src/$1',
+    '@/(.+)$': '<rootDir>/src/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  moduleDirectories: ['node_modules', 'src'],
+  modulePaths: ['node_modules', '<rootDir>/src'],
+  roots: ['<rootDir>/src'],
 }
 
 export default config
